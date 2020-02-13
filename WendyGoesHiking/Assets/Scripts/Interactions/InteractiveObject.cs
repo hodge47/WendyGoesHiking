7986fa88efcb3417cda120cf44a504ab24593367
Andrew Feagley <andrew.feagley@loop.colum.edu>
2020-02-12 19:07:42 -0600

    Adds interactive objects
    interactive objects and toggle objects
    up to video 11

---
+++ WendyGoesHiking/Assets/Scripts/Interactions/InteractiveObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour, IInteractive
+{
+    [Tooltip("Name of the interactive object the UI will display.")]
+    [SerializeField]
+    protected string displayText = nameof(InteractiveObject);
+    public string DisplayText => displayText;
+
+    public virtual void InteractWith()
+    {
+        Debug.Log($"Player just interacted with: {gameObject.name}");
+    }
+
+}
