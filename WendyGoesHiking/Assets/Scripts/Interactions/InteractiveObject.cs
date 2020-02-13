4ef0435c65cade65d1031c72258f23e5af17e2d6
Andrew Feagley <andrew.feagley@loop.colum.edu>
2020-02-13 17:47:45 -0600

    Adds inventory objects
    lets player pick up objects and add them to internal inventory
    lets player use objects in their inventory and have them removed
    lets player open and close doors
    lets player toggle objects on and off

--- WendyGoesHiking/Assets/Scripts/Interactions/InteractiveObject.cs
+++ WendyGoesHiking/Assets/Scripts/Interactions/InteractiveObject.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
-    [Tooltip("Name of the interactive object the UI will display.")]
+    [Tooltip("Name of the interactive object the UI will display when looked at in the world.")]
     [SerializeField]
     protected string displayText = nameof(InteractiveObject);
-    public string DisplayText => displayText;
+    public virtual string DisplayText => displayText;
 
+    /// <summary>
+    /// This is where audio should probably go.
+    /// </summary>
     public virtual void InteractWith()
     {
+        //Put audio play in here (probably)
+        //I didn't implement it
+        //Audio people should do it
+
+
         Debug.Log($"Player just interacted with: {gameObject.name}");
     }
 
