bcf95848a036777dac47d1ef4a00fef5217e0c16
loreleim <lorelei.miyamura@gmail.com>
2020-02-14 11:32:10 -0600

    Frontend HealthBar

---
+++ WendyGoesHiking/Assets/UI/Health.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+
+
+}
