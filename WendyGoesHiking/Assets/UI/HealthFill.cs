99c7456a74fc1697967bb6037b7af70540e77d93
loreleim <lorelei.miyamura@gmail.com>
2020-02-14 11:39:29 -0600

    Health Slider Fill with Backend

---
+++ WendyGoesHiking/Assets/UI/HealthFill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthFill : MonoBehaviour
+{
+    public Slider slider;
+
+    public void maxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void setHealth (int health)
+    {
+        slider.value = health;
+    }
+}
