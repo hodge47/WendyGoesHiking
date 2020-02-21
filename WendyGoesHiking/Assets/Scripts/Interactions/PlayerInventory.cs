a95466779098032e56a36567df741476c292faa4
loreleim <lorelei.miyamura@gmail.com>
2020-02-21 13:30:19 -0600

    making room for react project

--- WendyGoesHiking/Assets/Scripts/Interactions/PlayerInventory.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class PlayerInventory
-{
-    public static List<InventoryObject> InventoryObjects { get; } = new List<InventoryObject>();
-}
