54742b520e6cc8ad089ab0a3804f32f2dcbd5b71
jamespost <32137126+jamespost@users.noreply.github.com>
2020-02-23 17:33:04 -0600

    Update DynamicAmbienceManager.cs
    -first attempt at detecting game objects with specific tags and adding/removing them based off of proximity

--- WendyGoesHiking/Assets/Scripts/Audio/DynamicAmbienceManager.cs
+++ WendyGoesHiking/Assets/Scripts/Audio/DynamicAmbienceManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DynamicAmbienceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    ///jpost Audio
+    ///A class inspired by the idea of "frustrum culling" meant to dynamically instantiate and manage the lifecycle of FMOD ambience events
+    ///based on proximity to the player character
+    ///
+
+    //a list of gameObjects
+    [SerializeField] List<GameObject> nearbyGameObjects;
+
+    private void Start()
     {
-        
+        nearbyGameObjects = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //a way of detecting if a nearbyGameObject is tagged "Tree"
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Tree")
+        {
+            nearbyGameObjects.Add(other.gameObject);
+        }
+    }
+    //a way of detecting if a nearbyGameObject is tagged "Tree" and the player has moved away from it
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Tree")
+        {
+            nearbyGameObjects.Remove(other.gameObject);
+        }
     }
 }
