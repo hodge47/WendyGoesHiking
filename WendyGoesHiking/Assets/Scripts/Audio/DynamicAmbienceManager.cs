0552ca0320cc08fe6d87a85f8c28a69c45ad7e0b
jamespost <32137126+jamespost@users.noreply.github.com>
2020-03-06 10:37:36 -0600

    tweaks to dynamic ambience handler

--- WendyGoesHiking/Assets/Scripts/Audio/DynamicAmbienceManager.cs
+++ WendyGoesHiking/Assets/Scripts/Audio/DynamicAmbienceManager.cs
@@ -15,7 +15,7 @@ public class DynamicAmbienceManager : MonoBehaviour
     private void Start()
     {
         nearbyGameObjects = new List<GameObject>();
-        InvokeRepeating("CheckNearbyGameObjects", 1f, Random.Range(5f, 10f));
+        InvokeRepeating("CheckNearbyGameObjects", 1f, Random.Range(2f, 20f));
     }
 
     //a way of detecting if a nearbyGameObject is tagged "Tree"
@@ -24,7 +24,7 @@ public class DynamicAmbienceManager : MonoBehaviour
         if(other.gameObject.tag == "Tree")
         {
             nearbyGameObjects.Add(other.gameObject);
-            PlayTreeAmbience(other.gameObject);
+            //PlayTreeAmbience(other.gameObject);
         }
     }
 
