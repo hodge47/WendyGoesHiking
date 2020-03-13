f76b1dfa22dd7cca7cd650ac8fff5e4677d9c57d
jamespost <32137126+jamespost@users.noreply.github.com>
2020-03-13 15:38:24 -0500

    Update TriggerFMODOneShot.cs

--- WendyGoesHiking/Assets/Scripts/Audio/TriggerFMODOneShot.cs
+++ WendyGoesHiking/Assets/Scripts/Audio/TriggerFMODOneShot.cs
@@ -9,12 +9,49 @@ public class TriggerFMODOneShot : MonoBehaviour
     ///
 
     //box collider
-    public BoxCollider boxCollider;
+    private BoxCollider boxCollider;
+    //the tag (string) of the collider to trigger this event
+    public string triggerObject;
+    //the name (string) of the FMOD event to play
+    public string eventPath;
+    //the gameobject that the FMOD event should play at
+    public GameObject playBackObject;
 
+    //serialized fields
+    [SerializeField] bool playEventAtMainCamera = true;
+    [SerializeField] bool playerTrigger = true;
 
     private void Start()
     {
         //initialize boxCollider to be the gameObject's box collider
         boxCollider = gameObject.GetComponent<BoxCollider>();
+        //initialize event name to default empty string
+        eventPath = "";
+        //initialize play back object to default player camera
+        if (playEventAtMainCamera)
+        {
+            playBackObject = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+        //initialze triggerObject to be the player if playerTrigger is true
+        if (playerTrigger)
+        {
+            triggerObject = "Player";
+        }
+    }
+    
+    //method for detecting trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the gameobject collides with the triggerObject
+        if (other.CompareTag(triggerObject))
+        {
+            PlayFMODOneShot();
+        }
+    }
+
+    //a method to play the desired FMOD event at the location set by 
+    public void PlayFMODOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(eventPath, playBackObject.transform.position);
     }
 }
