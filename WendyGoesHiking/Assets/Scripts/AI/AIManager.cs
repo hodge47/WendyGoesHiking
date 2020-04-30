aff94d5c5eb5a604f596816e56a3f73f5c75d87a
Michael <michaelhdg03@gmail.com>
2020-04-30 15:29:27 -0500

    Adds random wendigo sightings to AIManager.cs

--- WendyGoesHiking/Assets/Scripts/AI/AIManager.cs
+++ WendyGoesHiking/Assets/Scripts/AI/AIManager.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using Sirenix.OdinInspector;
 
+public enum SightingType { PASSIVE, AGGRESSIVE, BOTH}
 public class AIManager : MonoBehaviour
 {
     [FoldoutGroup("Health")]
@@ -12,6 +14,14 @@ public class AIManager : MonoBehaviour
     [FoldoutGroup("Health")]
     [SerializeField]
     private bool destroyGameObjectOnKill = false;
+
+    [FoldoutGroup("Sightings")]
+    [Tooltip("The random time range for when the wendigo shows up in minutes.")]
+    [MinMaxSlider(0f, 50f, showFields: true)]
+    public Vector2 SightingTimeRange = new Vector2(3f, 5f);
+    [FoldoutGroup("Sightings")]
+    [Tooltip("This is the type of sighting that could happen - passive, attack, or both.")]
+    public SightingType SightingType = SightingType.PASSIVE;
 
     [FoldoutGroup("Testing")]
     [SerializeField]
@@ -28,11 +38,28 @@ public class AIManager : MonoBehaviour
     private AIAggressiveDash aggressiveDashAI;
     private AITreeJumping treeJumpingAI;
     private NavMeshAgent navMeshAgent;
-
     private bool activeMovement = false;
+
+    System.Random random;
+    private float sightingElapsedTime = 0f;
+    private float randomSightingTime = 0f;
+
+    private void OnGUI()
+    {
+        if(isTesting)
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 24;
+            style.normal.textColor = Color.green;
+            GUI.color = Color.green;
+            GUI.Label(new Rect(Screen.width - 90, Screen.height - 60, 100, 20), String.Format("{0:00.00}", sightingElapsedTime), style);
+        }
+    }
 
     private void Start()
     {
+        // Create new random
+        random = new System.Random();
         // Create the health script for the AI
         healthAI = AI.gameObject.AddComponent<AIHealth>();
         // Get the necessary components from the AI gameObject
@@ -45,8 +72,10 @@ public class AIManager : MonoBehaviour
         // Make sure the wendigo is alive
         healthAI.IsAlive = true;
         healthAI.DestroyGameObjectOnDeath = destroyGameObjectOnKill;
+        // Get random sighting time
+        randomSightingTime = SetRandomTime();
         // Hide the AI
-        if(!isTesting)
+        if (!isTesting)
             Invoke(nameof(HideAIOnStart), 0.5f);
     }
 
@@ -59,6 +88,9 @@ public class AIManager : MonoBehaviour
         }
         else if(activeMovement == false && AI.activeSelf == true)
             HideAI();
+
+        // Need to update Random sightings
+        CheckElapsedSightingTime();
     }
 
     private bool CheckActiveDash()
@@ -124,6 +156,52 @@ public class AIManager : MonoBehaviour
         // Tell the AI to start the ground dashing
         //groundAI.Dash();
     }
+    private float SetRandomTime()
+    {
+        // Get the random time in minutes from range
+        float _randomTime = random.Next((int)SightingTimeRange.x, (int)SightingTimeRange.y + 1);
+        // Convert to seconds
+        _randomTime *= 60;
+
+        return _randomTime;
+    }
+
+    public void AdjustRandomSightings(float _minTime, float _maxTime, SightingType _sightingType)
+    {
+        SightingTimeRange = new Vector2(_minTime, _maxTime);
+        SightingType = _sightingType;
+    }
+
+    private void CheckElapsedSightingTime()
+    {
+        if(sightingElapsedTime > randomSightingTime)
+        {
+            // Trigger ground dashing
+            switch(SightingType)
+            {
+                case SightingType.PASSIVE:
+                    TriggerAIGroundDashing(WendigoState.PASSIVE);
+                    break;
+                case SightingType.AGGRESSIVE:
+                    TriggerAIGroundDashing(WendigoState.AGGRESSIVE);
+                    break;
+                case SightingType.BOTH:
+                    WendigoState _ws = (random.Next(0, 2) == 0) ? WendigoState.PASSIVE : WendigoState.AGGRESSIVE; // 0 for passive, 1 for aggressive;
+                    TriggerAIGroundDashing(_ws);
+                    break;
+            }
+            Debug.Log($"{Enum.GetName(typeof(SightingType), SightingType)} sighting was triggered...");
+            // Get new random time
+            randomSightingTime = SetRandomTime();
+            // Set the elapsed time back to 0
+            sightingElapsedTime = 0f;
+        }
+        else
+        {
+            sightingElapsedTime += Time.deltaTime;
+        }
+    }
+
 
     public void ShowAI()
     {
