b726f1124d95223c846d39c7b92755d9f1bb266a
hhoare <henry.hoare@hotmail.com>
2020-04-30 18:05:05 -0400

    _

---
+++ WendyGoesHiking/Assets/Scripts/TransitionTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject BlackAndWhitePPVolume;
+    [SerializeField]
+    GameObject ColorPPVolume;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            ColorPPVolume.SetActive(true);
+            BlackAndWhitePPVolume.SetActive(false);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            BlackAndWhitePPVolume.SetActive(true);
+            ColorPPVolume.SetActive(false);
+        }
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
