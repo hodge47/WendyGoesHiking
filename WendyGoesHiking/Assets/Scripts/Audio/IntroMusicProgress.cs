51bfefc6f1df198a084509bee1d5a73f737562d9
jamespost <32137126+jamespost@users.noreply.github.com>
2020-05-01 18:04:43 -0500

    implemented intro music in game
    intro music now plays and progresses along with the player in game

---
+++ WendyGoesHiking/Assets/Scripts/Audio/IntroMusicProgress.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class IntroMusicProgress : MonoBehaviour
+{
+    //jpost Audio
+
+    ///A script to pass a parameter value from Unity to FMOD that controls what sections of a song to play based on player progression
+    ///
+
+    public float introProgress;
+    
+    public string introMusic = "event:/Music/Intro/mx_wgh_intro";
+    public FMOD.Studio.EventInstance introMusicEvent;
+    public FMOD.Studio.PARAMETER_ID introProgressParameterId;
+
+    private void Start()
+    {
+        introProgress = 0;
+
+        introMusicEvent = FMODUnity.RuntimeManager.CreateInstance(introMusic);
+        introMusicEvent.getParameterByName("introProgress", out introProgress);
+        introMusicEvent.start();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.name)
+        {
+            case "Intro Music Progress Zone 1":
+                introProgress = 0;                
+                break;
+            case "Intro Music Progress Zone 2":
+                introProgress = 10;
+                break;
+            case "Intro Music Progress Zone 3":
+                introProgress = 20;
+                break;
+            case "Intro Music Progress Zone 4":
+                introProgress = 30;
+                break;
+            case "Intro Music Progress Zone 5":
+                introProgress = 0;
+                introMusicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                break;
+        }
+        introMusicEvent.setParameterByName("introProgress", introProgress);
+    }
+}
